37113e2d75e5de3d792cd1fe8cbb7da71854cb7f
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2026-03-30 15:29:11 +0530

    terraform learning

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "instance" {
-  ami           = data.aws_ami.ami.id
+  ami           = data.aws_ami.ami.image_id
   instance_type = "t3.small"
   vpc_security_group_ids = [var.sg_id]
 
